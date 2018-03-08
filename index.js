c1e24f37f5b7395c9ed138113e7593d8f43c8f93
Shaheer Mir <shaheermir0@gmail.com>
2018-03-08 13:53:30 -0500

    Adding eslint

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
+import store from './src/config/store'
 import App from './src/App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
