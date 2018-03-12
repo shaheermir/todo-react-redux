eecd5d7e74126bce8fad7e5fac768c5efa902b05
Shaheer Mir <shaheermir0@gmail.com>
2018-03-11 21:25:20 -0400

    Refactored middlewares

--- src/config/store.js
+++ src/config/store.js
@@ -1,49 +1,17 @@
-import { createStore } from 'redux'
+import ReduxLogger from 'redux-logger'
+import ReduxPromise from 'redux-promise'
+import { createStore, applyMiddleware } from 'redux'
+
 import reducer from '../reducers'
 
-const logger = store => next => {
-  if (!console.group) {
-    return next
-  }
-
-  return action => {
-    console.group(action.type)
-    console.log('%c Previous State', 'color: gray', store.getState())
-    console.log('%c Action', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c Next State', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-const promise = store => next => action => {
-  if (typeof action.then === 'function') {
-    return action.then(next)
-  }
-  return next(action)
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares
-    .slice()
-    .reverse()
-    .forEach(middleware => {
-      store.dispatch = middleware(store)(store.dispatch)
-    })
-}
-
 const configureStore = () => {
-  const store = createStore(reducer)
-  const middlewares = [promise]
+  const middlewares = [ReduxPromise]
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger)
+    middlewares.push(ReduxLogger)
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares)
-
-  return store
+  return createStore(reducer, applyMiddleware(...middlewares))
 }
 
 export default configureStore
